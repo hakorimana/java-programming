2f5fc2f623bebb0942689baed9cf004753087fb3
Think <Think@172.31.165.19>
2022-09-23 11:55:42 +0200

    you can work hardly

---
+++ src/main/java/ur/arraylist/firstArr.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ur.arraylist;
+
+/**
+ *
+ * @author Think
+ */
+public class firstArr{
+        public static void main(String[] args){
+                int[] grade=new int[10];
+                grade[0]=34;
+                System.out.print("first index "+grade[0]);
+                System.out.print("hello word");
+}
+    
+}
