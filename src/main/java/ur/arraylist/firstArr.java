864b8b4e37a17aaaaeacb221c5a4f2e6ce50c847
Think <Think@172.31.165.19>
2022-09-24 10:27:03 +0200

    continue working

--- src/main/java/ur/arraylist/firstArr.java
+++ src/main/java/ur/arraylist/firstArr.java
@@ -11,10 +11,11 @@ package ur.arraylist;
  */
 public class firstArr{
         public static void main(String[] args){
-                int[] grade=new int[10];
+                int[] grade={0,5,6,67,23,3,4,7,8};
                 grade[0]=34;
-                System.out.print("first index "+grade[0]);
+                System.out.print("first index "+grade[0]+"and index 4"+grade[3]);
                 System.out.print("hello word");
+                
 }
     
 }
